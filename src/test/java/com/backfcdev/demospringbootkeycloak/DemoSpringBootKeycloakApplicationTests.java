670576a50d172d33e276daeccb1aeeceff456262
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-08-06 09:39:52 -0500

    add project app-spring-boot-keycloak

---
+++ src/test/java/com/backfcdev/demospringbootkeycloak/DemoSpringBootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backfcdev.demospringbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
