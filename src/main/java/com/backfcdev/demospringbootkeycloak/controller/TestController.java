fdf1faf727da10a9b65adbb9ee8acef16719cc79
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-08-18 21:49:11 -0500

    exposing different endpoints in controller

---
+++ src/main/java/com/backfcdev/demospringbootkeycloak/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.backfcdev.demospringbootkeycloak.controller;
+
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/everyone-authenticated")
+    public String getEveryoneText() {
+        return "hello to everyone that is authenticated";
+    }
+
+    @GetMapping("/user")
+    @RolesAllowed("USER")
+    public String getUserText() {
+        return "hello user";
+    }
+
+    @GetMapping("/admin")
+    @RolesAllowed("ADMIN")
+    public String getAdminText() {
+        return "hello admin";
+    }
+}
