fc8d153a50919f9b9cf76a6111d5984619c9917b
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-08-18 21:48:02 -0500

    rules to authorize HTTP requests only to authenticated users and configures authentication based on JWT tokens, turning token roles into security authorities. It also establishes a session handling strategy and records the necessary configuration for the OAuth2 resource server.

---
+++ src/main/java/com/backfcdev/demospringbootkeycloak/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.backfcdev.demospringbootkeycloak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true) // allows using the @RolesAllowed annotation
+public class WebSecurityConfig {
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /* any endpoint that does not require a specific role (is not having @RolesAllowed annotation)
+         can be accessed only by authenticated user */
+        http.authorizeHttpRequests(authRequest ->
+                authRequest.anyRequest().authenticated());
+
+        // add configuration of authorization based on roles to oauth2ResourceServer
+        http.oauth2ResourceServer(oauthResource ->
+                oauthResource.jwt(jwtConfigurer ->
+                                jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
+        return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new GrantedAuthoritiesConverter());
+        return jwtConverter;
+    }
+
+}
